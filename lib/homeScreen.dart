3aa0c8da0fdb10b60eb9455e195c6df26f7b99f5
mehul <mehulzinzuvadiya098@gmail.com>
2023-01-01 16:54:01 +0530

    done

--- lib/homeScreen.dart
+++ lib/homeScreen.dart
@@ -1,5 +1,4 @@
 import 'dart:math';
-
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -10,7 +9,7 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  List l1 = [
+  List imglist = [
     "assets/images/dice.png",
     "assets/images/dice1.png",
     "assets/images/dice2.png",
@@ -18,40 +17,99 @@ class _HomePageState extends State<HomePage> {
     "assets/images/dice4.png",
     "assets/images/dice5.png",
   ];
+
+  List l1 = [];
   int i = 0, sum = 0;
+  List rndlist = [];
+  TextEditingController txtdata = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-        child: Scaffold(
-      appBar: AppBar(
-        title: Text("LudoKIng"),
-        backgroundColor: Colors.amber,
-        centerTitle: true,
-        leading: Icon(Icons.menu),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Center(
-            child: InkWell(
-                onTap: () {
-                  var R = Random();
-                  setState(() {
-                    i = R.nextInt(6);
-                    print("$i");
-                    sum = sum + i + 1;
-                    print("$sum");
-                  });
+      child: Scaffold(
+        appBar: AppBar(
+            title: Text("Dice App",
+                style: TextStyle(fontSize: 25, color: Colors.white)),
+            centerTitle: true,
+            backgroundColor: Colors.amber),
+        body: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(20),
+              child: TextField(
+                controller: txtdata,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  label: Text(
+                    "Enter how many dice",
+                    style: TextStyle(color: Colors.amber),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.amber),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.red, width: 2),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  l1.clear();
+                  rndlist.clear();
+                  var data = txtdata.text;
+                  int n = int.parse(data);
+
+                  for (int i = 0; i < n; i++) {
+                    setState(() {
+                      l1.add(0);
+                    });
+                  }
                 },
-                child: Image.asset(
-                  l1[i],
-                  height: 100,
-                  width: 100,
-                )),
-          ),
-        ],
+                child: Text("Generate")),
+            SizedBox(
+              height: 20,
+            ),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: l1
+                    .asMap()
+                    .entries
+                    .map((e) => Padding(
+                      padding: const EdgeInsets.all(5),
+                      child: Image.asset(
+                            imglist[rndlist.isEmpty ? 0 : rndlist[e.key]],
+                            height: 50,
+                            width: 50,
+                          ),
+                    ))
+                    .toList(),
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  rndlist.clear();
+                  var rnd = Random();
+                  int n = int.parse(txtdata.text);
+                  for (i = 0; i < n; i++) {
+                    var j = rnd.nextInt(6);
+                    setState(() {
+                      rndlist.add(j);
+                    });
+                  }
+                },
+                child: Text("Play"))
+          ],
+        ),
       ),
-    ));
+    );
   }
 }
